f9f916742b261d8b35cafae59bfab5affed5593d
Enzo Garcia <enzoocezargarcia@gmail.com>
2025-03-31 17:29:10 -0300

    refactoring-components

---
+++ lib/components/transaction_item.dart
@@ -0,0 +1,71 @@
+import 'package:flutter/material.dart';
+import '../models/transaction.dart';
+import 'package:intl/intl.dart';
+
+class TransactionItem extends StatelessWidget {
+  const TransactionItem({
+    super.key,
+    required this.tr,
+    required this.onRemove,
+  });
+
+  final Transaction tr;
+  final void Function(String p1) onRemove;
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      elevation: 10,
+      margin: EdgeInsets.symmetric(
+        vertical: 5,
+        horizontal: 10,
+      ),
+      child: ListTile(
+          leading: CircleAvatar(
+            radius: 30,
+            backgroundColor: Theme.of(context).colorScheme.primary,
+            child: Padding(
+              padding: const EdgeInsets.all(6.0),
+              child: FittedBox(
+                child: Text(
+                  'R\$${tr.value.toStringAsFixed(2)}',
+                  style: TextStyle(
+                    color: Colors.white, // Theme.of(context).colorScheme.secondary,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+              ),
+            ),
+          ),
+          title: Text(
+            tr.title,
+            style: TextStyle(
+              fontSize: 16,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+          subtitle: Text(
+            DateFormat('d MMM y').format(tr.date),
+          ),
+          trailing: MediaQuery.of(context).size.width > 480
+              ? TextButton.icon(
+                  onPressed: () => onRemove(tr.id),
+                  label: Text(
+                    'Excluir',
+                    style: TextStyle(color: Theme.of(context).colorScheme.error),
+                  ),
+                  icon: Icon(
+                    Icons.delete,
+                    color: Theme.of(context).colorScheme.error,
+                  ),
+                )
+              : IconButton(
+                  onPressed: () => onRemove(tr.id),
+                  icon: Icon(Icons.delete),
+                  color: Theme.of(context).colorScheme.error,
+                  splashRadius: 25,
+                  splashColor: Color.fromARGB(255, 243, 176, 182),
+                )),
+    );
+  }
+}
--- lib/components/transaction_list.dart
+++ lib/components/transaction_list.dart
@@ -1,6 +1,6 @@
 import 'package:expenses_app/models/transaction.dart';
 import 'package:flutter/material.dart';
-import 'package:intl/intl.dart';
+import 'transaction_item.dart';
 
 class TransactionList extends StatelessWidget {
   const TransactionList(this.transactions, this.onRemove, {super.key});
@@ -15,7 +15,7 @@ class TransactionList extends StatelessWidget {
             builder: (ctx, constraints) {
               return Column(
                 children: [
-                  SizedBox(height: 10),
+                  const SizedBox(height: 10),
                   Text(
                     'Não há transações cadastradas.',
                     style: TextStyle(
@@ -24,7 +24,7 @@ class TransactionList extends StatelessWidget {
                       color: Color.fromARGB(252, 255, 255, 255),
                     ),
                   ),
-                  SizedBox(height: 20),
+                  const SizedBox(height: 20),
                   SizedBox(
                     height: constraints.maxHeight * 0.5,
                     child: Image.asset(
@@ -42,59 +42,7 @@ class TransactionList extends StatelessWidget {
               itemCount: transactions.length,
               itemBuilder: (ctx, index) {
                 final tr = transactions[index];
-                return Card(
-                  elevation: 10,
-                  margin: EdgeInsets.symmetric(
-                    vertical: 5,
-                    horizontal: 10,
-                  ),
-                  child: ListTile(
-                      leading: CircleAvatar(
-                        radius: 30,
-                        backgroundColor: Theme.of(context).colorScheme.primary,
-                        child: Padding(
-                          padding: const EdgeInsets.all(6.0),
-                          child: FittedBox(
-                            child: Text(
-                              'R\$${tr.value.toStringAsFixed(2)}',
-                              style: TextStyle(
-                                color: Colors.white, // Theme.of(context).colorScheme.secondary,
-                                fontWeight: FontWeight.bold,
-                              ),
-                            ),
-                          ),
-                        ),
-                      ),
-                      title: Text(
-                        tr.title,
-                        style: TextStyle(
-                          fontSize: 16,
-                          fontWeight: FontWeight.bold,
-                        ),
-                      ),
-                      subtitle: Text(
-                        DateFormat('d MMM y').format(tr.date),
-                      ),
-                      trailing: MediaQuery.of(context).size.width > 480
-                          ? TextButton.icon(
-                              onPressed: () => onRemove(tr.id),
-                              label: Text(
-                                'Excluir',
-                                style: TextStyle(color: Theme.of(context).colorScheme.error),
-                              ),
-                              icon: Icon(
-                                Icons.delete,
-                                color: Theme.of(context).colorScheme.error,
-                              ),
-                            )
-                          : IconButton(
-                              onPressed: () => onRemove(tr.id),
-                              icon: Icon(Icons.delete),
-                              color: Theme.of(context).colorScheme.error,
-                              splashRadius: 25,
-                              splashColor: Color.fromARGB(255, 243, 176, 182),
-                            )),
-                );
+                return TransactionItem(tr: tr, onRemove: onRemove);
               },
             ),
           );
